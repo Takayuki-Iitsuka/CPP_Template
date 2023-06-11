294d120b862bd1619253765dbd0b2f6fe247f18a
takayuki.iitsuka <takayuki.iitsuka@gmail.com>
2023-06-11 15:35:56 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,27 @@
+﻿// main.cpp
+
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+
+#define MY_PI 3.141592653589793238462643
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+
+
+	cout << "Hello World!" << endl;
+	cout << "C++ Template..............!" << endl;
+	cout << MY_PI << endl;
+
+#ifdef _DEBUG
+	system("pause");
+	//char hoge = getchar();
+#endif
+
+	return 0;
+}
+
+// End
